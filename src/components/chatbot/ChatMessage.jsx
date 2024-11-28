ed658fde7115fdbe1e6acb7a78b5306d17223097
enble <enble_777@naver.com>
2024-11-28 23:46:12 +0900

    feat: 이미지가 보여지도록 수정

--- src/components/chatbot/ChatMessage.jsx
+++ src/components/chatbot/ChatMessage.jsx
@@ -2,13 +2,14 @@ import React from "react";
 import PropTypes from 'prop-types';
 import "./ChatMessage.css";
 
-const ChatMessage = ({ message, isUser, timestamp }) => {
+const ChatMessage = ({ message, isUser, timestamp, imageUrl }) => {
   return (
     <div className="message-container">
       <div className={`message-content ${isUser ? 'user-message' : 'bot-message'}`}>
         {!isUser && <span className="message-time">{timestamp}</span>}
         <div className={`message-bubble ${isUser ? 'user-bubble' : 'bot-bubble'}`}>
           {message}
+          {imageUrl && <img src={imageUrl} alt="Generated" className="generated-image" />}
         </div>
         {isUser && <span className="message-time">{timestamp}</span>}
       </div>
@@ -19,7 +20,8 @@ const ChatMessage = ({ message, isUser, timestamp }) => {
 ChatMessage.propTypes = {
   message: PropTypes.string.isRequired,
   isUser: PropTypes.bool.isRequired,
-  timestamp: PropTypes.string.isRequired
+  timestamp: PropTypes.string.isRequired,
+  imageUrl: PropTypes.string
 };
 
 export default ChatMessage;
